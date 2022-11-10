4cd8629d8901f990e8d8ac403b1a26edfb466948
toga4 <81744248+toga4@users.noreply.github.com>
2022-11-11 00:02:02 +0900

    verify with backend api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,23 @@
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import liff from "@line/liff";
 import "./App.css";
 
 function App() {
   const [message, setMessage] = useState("");
-  const [idToken, setIdToken] = useState("");
+  const [idToken, setIdToken] = useState<string | null>("");
+  const [idTokenClaims, setIdTokenClaims] = useState("");
   const [error, setError] = useState("");
 
-  useEffect(() => {
+  useState(() => {
     liff
       .init({
         liffId: import.meta.env.VITE_LIFF_ID,
         mock: import.meta.env.VITE_LIFF_MOCK === "true",
+        withLoginOnExternalBrowser: true,
       })
       .then(async () => {
         setMessage("LIFF init succeeded.");
-        if (!liff.isLoggedIn()) {
-          liff.login();
-        }
-        setIdToken(liff.getIDToken() || "");
+        setIdToken(liff.getIDToken());
       })
       .catch((e: Error) => {
         setMessage("LIFF init failed.");
@@ -26,23 +25,63 @@ function App() {
       });
   });
 
+  const onClickVerify = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "https://toga4-liff-sandbox-ki6hejsv5q-an.a.run.app/verify",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ idToken }),
+        }
+      );
+
+      if (response.ok) {
+        const resp = await response.json();
+        setIdTokenClaims(JSON.stringify(resp, null, "  "));
+      } else {
+        setMessage("ID token verify failed.");
+      }
+    } catch (e) {
+      setError(`${e}`);
+    }
+  }, [idToken]);
+
   return (
     <div className="App">
-      <h1>create-liff-app</h1>
+      <h1>toga4-liff-sandbox</h1>
       {message && <p>{message}</p>}
-      {idToken && <p>{idToken}</p>}
       {error && (
         <p>
           <code>{error}</code>
         </p>
       )}
-      <a
-        href="https://developers.line.biz/ja/docs/liff/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        LIFF Documentation
-      </a>
+      <p>
+        <a
+          href="https://github.com/toga4/liff-sandbox"
+          target="_blank"
+          rel="noreferrer"
+        >
+          github.com/toga4/liff-sandbox
+        </a>
+      </p>
+      {idToken && (
+        <>
+          <p>
+            <textarea readOnly>{idToken}</textarea>
+          </p>
+          <p>
+            <button onClick={onClickVerify}>verify</button>
+          </p>
+        </>
+      )}
+      {idTokenClaims && (
+        <p>
+          <pre>{idTokenClaims}</pre>
+        </p>
+      )}
     </div>
   );
 }
